79038649dcde9dc0a02cda64663d3af3402f425a
Tyonar <ti-mousse-ett@hotmail.com>
2015-05-27 08:50:24 -0400

    Updated Tp4Test to test tax floor

--- app/src/androidTest/java/com/marc/arseneault/tp4/Tp4Test.java
+++ app/src/androidTest/java/com/marc/arseneault/tp4/Tp4Test.java
@@ -7,15 +7,12 @@ import junit.framework.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import model.Product;
 import service.RepositoryService;
 import service.RepositoryServiceInterface;
 
-
-/**
- * v1.0
- */
 public class Tp4Test extends AndroidTestCase{
 
     RepositoryServiceInterface repoServ;
@@ -43,26 +40,60 @@ public class Tp4Test extends AndroidTestCase{
 
     public void testTfo2Items() {
         Product product = new Product("Avocat", "123456", 12.54, true);
-        ProductItem p = new ProductItem(1, product);
+        ProductItem p = new ProductItem(2, product);
         items.add(p);
 
-        Assert.assertEquals("Test deux pour un 2 items failed", 12.54, repoServ.GetTotalPrice(items, 10000));
+        Assert.assertEquals("Test deux pour un 2 items failed", 12.54, repoServ.GetTotalPrice(items, 0));//0 pour que les taxes n'influencent pas les calculs
     }
 
     public void testTfo1Item() {
         Product product = new Product("Avocat", "123456", 12.54, true);
 
-        TP4Activity activity = new TP4Activity();
-        activity.adapter.add(new ProductItem(1, product));
-        Assert.assertEquals("Test deux pour un 1 items failed", 12.54, repoServ.GetTotalPrice(items, 100000));
+        items.add(new ProductItem(1, product));
+        Assert.assertEquals("Test deux pour un 1 items failed", 12.54, repoServ.GetTotalPrice(items, 0));//0 pour que les taxes n'influencent pas les calculs
     }
 
     public void testTfo3Items() {
         Product product = new Product("Avocat", "123456", 12.54, true);
 
-        TP4Activity activity = new TP4Activity();
-        activity.adapter.add(new ProductItem(3, product));
-        Assert.assertEquals("Test deux pour un 2 items failed", 12.54 * 2, repoServ.GetTotalPrice(items, 100000));
+        items.add(new ProductItem(3, product));
+        Assert.assertEquals("Test deux pour un 2 items failed", 12.54 * 2, repoServ.GetTotalPrice(items, 0));//0 pour que les taxes n'influencent pas les calculs
+    }
+
+    public void testTaxes()
+    {
+        Product product = new Product("Avocat", "123456", 12.54, false);
+
+        items.add(new ProductItem(1, product));
+        Assert.assertEquals("Test taxes failed", (12.54 * 1.15) * 1.15, repoServ.GetTotalPrice(items, 100000));//10000 pour quil ne puisse se faire atteindre, on veut tester les taxes
+    }
+
+    public void testTaxesOver100()
+    {
+        Product product = new Product("Avocat", "123456", 50, false);
+
+        items.add(new ProductItem(3, product));
+        Assert.assertEquals("Test taxes failed", 150.0, repoServ.GetTotalPrice(items, 100));
+    }
+
+    public void testTaxesOver100Random()
+    {
+        Product product = new Product("Avocat", "123456", 50, false);
+        Random rand = new Random(123456);
+
+        for(int i = 0; i < 100; i++)
+        {
+            int count = rand.nextInt(100);
+            items.add(new ProductItem(count, product));
+            if(count * 50 > 500)
+            {
+                Assert.assertEquals("testTaxesOver100Random failed when count = " + count, count * 50, repoServ.GetTotalPrice(items, 500));
+            }
+            else
+            {
+                Assert.assertEquals("testTaxesOver100Random failed when count = " + count, ((count * 50)*1.15)*1.15, repoServ.GetTotalPrice(items, 500));
+            }
+        }
     }
 
 }
